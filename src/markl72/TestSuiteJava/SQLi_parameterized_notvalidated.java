034e7307b9da3bb3a7549b78860690b46f81faba
markl72 <non_111111@hotmail.com>
2022-04-02 21:33:00 +1100

    annotated vulnerabilities

--- src/markl72/TestSuiteJava/SQLi_parameterized_notvalidated.java
+++ src/markl72/TestSuiteJava/SQLi_parameterized_notvalidated.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,18 +23,18 @@ public class SQLi_parameterized_notvalidated extends HttpServlet {
 
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		
-		response.setContentType("text/html;charset=UTF-8");
+		response.setContentType("text/html"); // #1 Doesn't set character set
         PrintWriter out = response.getWriter();
         
-        out.println("<HTML><BODY><p><b>Concatenated and not validated</b></p>");
+        out.println("<HTML><BODY><p><b>Parameterized query and not validated</b></p>");
         
         // print form
         out.println("<p>Enter your credentials to log in</p>" 
-        		+ "<FORM action='" + request.getContextPath() + "/SQLi3' method='POST'>"
+        		+ "<FORM action='" + request.getContextPath() + "/SQLi4' method='POST'>"
         		+ "userid: <input name='userid'>"
         		+ "<br>password: <input name='password'>"
         		+ "<br><input type='submit'>"
-        		+ "</BODY></HTML>");
+        		+ "</BODY></HTML>"); // #2 No autocomplete on form
         
 		//doPost(request, response);
 	}
@@ -47,33 +48,37 @@ public class SQLi_parameterized_notvalidated extends HttpServlet {
 		String userid = request.getParameter("userid");
 		String password = request.getParameter("password");
 		
-		out.println("<p>Submitted userid: " + userid + "<BR>");
-		out.println("Submitted password: " + password + "</p>");
+		// #3 userid parameter not validated
+		// #4 password parameter not validated
+		
+		out.println("<p>Submitted userid: " + userid + "<BR>");	// #5 Reflected XSS
+		out.println("Submitted password: " + password + "</p>"); // #6 Reflected XSS
 		
 		try {
 			
 			Class.forName("com.mysql.jdbc.Driver");  
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/insecureapp","insecureapp","45EUlZOpL7");  
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/insecureapp","insecureapp","45EUlZOpL7");  // #7 Hardcoded password
 			
 			String sql = "SELECT * FROM users WHERE userid=? AND password=?";
 			
-        	out.println("<p><b>SQL:</b> " + sql + "</p>");
+        	out.println("<p><b>SQL:</b> " + sql + "</p>"); // #8 Reflected XSS 
         	out.println("<p><b>Results:</b></p>");
   
-			PreparedStatement pstmt = connection.prepareStatement(sql);
+			PreparedStatement pstmt = connection.prepareStatement( sql ); 
 			pstmt.setString(1,userid);
 			pstmt.setString(2,password);
             ResultSet rs = pstmt.executeQuery(sql);  
             
             while(rs.next()) {
-            	out.println("<p>Name: " + rs.getString(3) + " " + rs.getString(2) + ", Address: " + rs.getString(4) + ", Phone no: " + rs.getString(5) + "</p>");
+            	out.println("<p>Name: " + rs.getString(3) + " " + rs.getString(2) + ", Address: " + rs.getString(4) + ", Phone no: " + rs.getString(5) + "</p>"); // #10 Stored XSS
             }
             connection.close();
             out.println("</BODY></HTML>");
             
         } 
 		catch(Exception e){ 
-			out.println(e);
+			out.println(e); // #11 Stack trace
+			System.out.println(e);
 		}  		
 	}
 }
